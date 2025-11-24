740778ba3a9ec4d615019281c65a52774ba9314e
cleaver-clover <untilhardwiredsubdivide@proton.me>
2025-11-24 13:34:33 +0000

    added more suggestive descripions for keybindings

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -19,22 +19,22 @@ vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete to blackhole register"
 vim.keymap.set({ "n", "v" }, "x", '"_x', { desc = "Delete char to blackhole register" })
 
 -- Spell check commands
-vim.keymap.set("n", "<leader>s<Left>", "[s", { desc = "Go to previous search match/error" })
-vim.keymap.set("n", "<leader>s<Right>", "]s", { desc = "Go to next search match/error" })
+vim.keymap.set("n", "<leader>s<Left>", "[s", { desc = "Spell: Go to previous search match/error" })
+vim.keymap.set("n", "<leader>s<Right>", "]s", { desc = "Spell: Go to next search match/error" })
 vim.keymap.set("n", "<leader>ss", function()
 	require("telescope.builtin").spell_suggest(require("telescope.themes").get_cursor({}))
 end, { desc = "Telescope spell suggestions" })
 
-vim.keymap.set("n", "<leader>sa", "zg", { desc = "Add word to dictionary" })
-vim.keymap.set("n", "<leader>sr", "zug", { desc = "Remove word from dictionary" })
-vim.keymap.set("n", "<leader>sb", "zw", { desc = "Add bad word to dictionary" })
-vim.keymap.set("n", "<leader>sbr", "zuw", { desc = "Remove bad word from dictionary" })
+vim.keymap.set("n", "<leader>sa", "zg", { desc = "Sepll: Add word to dictionary" })
+vim.keymap.set("n", "<leader>sr", "zug", { desc = "Sepll: Remove word from dictionary" })
+vim.keymap.set("n", "<leader>sb", "zw", { desc = "Sepll: Add bad word to dictionary" })
+vim.keymap.set("n", "<leader>sbr", "zuw", { desc = "Sepll: Remove bad word from dictionary" })
 
 -- Buffer commands
-vim.keymap.set("n", "<leader>w", "<C-w>", { desc = "Change visible buffer" })
-vim.keymap.set("n", "<leader>bk", ":bnext<CR>", { desc = "Change to next buffer" })
-vim.keymap.set("n", "<leader>bj", ":bprevious<CR>", { desc = "Change to previous buffer" })
-vim.keymap.set("n", "<leader>bq", ":bdelete<CR>", { desc = "Close current buffer" })
+vim.keymap.set("n", "<leader>w", "<C-w>", { desc = "Buffer: Change visible buffer" })
+vim.keymap.set("n", "<leader>bk", ":bnext<CR>", { desc = "Buffer: Change to next buffer" })
+vim.keymap.set("n", "<leader>bj", ":bprevious<CR>", { desc = "Buffer: Change to previous buffer" })
+vim.keymap.set("n", "<leader>bq", ":bdelete<CR>", { desc = "Buffer: Close current buffer" })
 
 -- git
 vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Git view changes" })
@@ -46,7 +46,7 @@ vim.keymap.set(
 	{ "n", "v" },
 	"<leader>t",
 	":Neotree filesystem reveal left<CR>",
-	{ desc = "Reveals Neotree on working directory" }
+	{ desc = "Neotree on working directory" }
 )
 
 -- LSP
@@ -62,11 +62,11 @@ vim.api.nvim_set_keymap(
 	"n",
 	"<leader>lc",
 	":!sh ./Compile.sh<CR>",
-	{ desc = "Make LaTeX project by executing Compile.sh" }
+	{ desc = "LaTeX: Compile project with Compile.sh" }
 )
 vim.api.nvim_set_keymap(
 	"n",
 	"<leader>lo",
 	":silent !ls *.pdf | xargs zathura &<CR>",
-	{ desc = "Open latex pdf with zathura" }
+	{ desc = "LaTeX: Open pdf with zathura" }
 )
